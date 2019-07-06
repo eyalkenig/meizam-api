1935551ca3b65d4e715ba1644801ef19f00410c0
Eyal Kenig <eyalkenig@gmail.com>
2019-07-06 11:24:11 +0300

    feat(services/meizam-api-service.ts):added fetch teams

---
+++ web/src/services/meizam-api-service.ts
@@ -0,0 +1,20 @@
+import axios from 'axios'
+import auth from '@/services/auth-service'
+
+class MeizamApi {
+  async fetchTeams (): Promise<any> {
+    return this.get('/teams')
+  }
+  private async get (path: string): Promise<any> {
+    console.log('env', JSON.stringify(process.env))
+    const accessToken = await auth.getAccessToken()
+    const basePath = process.env.VUE_APP_MEIZAM_API_BASE_URL
+    return axios.get(`${basePath}${path}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    })
+  }
+}
+
+export default new MeizamApi()
